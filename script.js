55131193d91d4936cb63eb44b95a1b405f0066e3
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-09-30 09:32:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
 
+  // Get the values of the first and last name input fields
+  const firstNameValue = form1.fname.value;
+  const lastNameValue = form1.lname.value;
+
+  // if (firstNameValue.trim() === "" || lastNameValue.trim() === "") {
+  //   alert("Please enter both first and last names.");
+  // } else {
+  // }
+    alert(`${firstNameValue} ${lastNameValue}`);
 }
